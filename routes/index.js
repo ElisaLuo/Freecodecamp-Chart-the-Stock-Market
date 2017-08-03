2e063b23309556715af95f750ed6c9c84d5e6cd8
Elisa L <luolixuan2003@gmail.com>
2017-08-03 01:27:39 +0000

    Chart is working

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,30 @@ const express = require('express');
 const router = express.Router();
 const googleFinance = require("google-finance");
 const moment = require('moment');
-const highcharts = require('highcharts');
 var data = [];
-var data2 =[];
 var date = new Date();
-googleFinance.historical({
-  symbols: ['GOOG', 'AAPL'],
-  from: '2017-07-01',
-  to: new Date()
-}, function (err, result) {
-	if(err) return err;
-	for(var i = 0; i < result.GOOG.length; i++){
-		data.push([Number(moment(result.GOOG[i].date, "MMMM D, YYYY").format("X"))*1000, result.GOOG[i].close]);
-	}
-	router.get('/', function(req, res){
-			res.render('index',{
-				data: data
-			});
-		});
-		router.get('/api/:stock', function(req, res){
-			res.json({data});
-		});
+
+router.get('/data/:stock', function(req, res){
+	googleFinance.historical({
+		symbol: req.params.stock,
+		from: '2013-01-01',
+		to: '2017-08-02'
+	}, function (err, quotes) {
+		if(err) return err;
+		for(var i = 0; i < quotes.length; i++){
+			data.push([Number(moment(quotes[i].date, "MMMM D, YYYY").format("X"))*1000, quotes[i].close]);
+		}
+		res.json({data});
+		data = [];
+	});
+	
 });
+
+router.get('/', function(req, res){
+	res.render('index', {
+		data: data
+	});
+});
+
 
 module.exports = router;
