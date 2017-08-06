61e281d1ded1355f6cd83e557ab2e68e4c61d020
Elisa L <luolixuan2003@gmail.com>
2017-08-06 15:53:51 +0000

    slight changes to filter system

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,54 @@ const router = express.Router();
 const googleFinance = require("google-finance");
 const moment = require('moment');
 const StockSymbolLookup = require('stock-symbol-lookup');
-var data = [];
-var stock = ['AAPL', 'GOOG', 'aa'];
-var name = [];
-var valid = [];
+var data = []; // data to be graphed
+var stocks = []; //invalid data
+var stock = ['AAPL', 'GOOG', 'aa']; //initial data
+var name = []; // data with desc
+var valid = []; // valid data
 
-function loadData(){
+function loadData(){ //looks up data
 	StockSymbolLookup.loadData()
     .then((data) => {
+    	name = []; //resets desc
+        for(var i = 0; i < data.securities.length; i++){ //if stock load up has data, pushes it into the name arr
+        	for(var j = 0; j < stock.filter(Boolean).length; j++){
+        		stock = stock.filter(Boolean);
+            	if(stock[j] == data.securities[i].symbol){
+                	name.push([data.securities[i].symbol, data.securities[i].securityName]);
+            	}
+        	}
+		}
+		valid = stock.filter(function(element){ // sets valid to the valid elements
+            for(var i = 0; i < data.securities.length; i++){
+                if(data.securities[i].symbol === element){
+                    return element;
+                }
+            }
+        });
+        stocks = stock.filter(function(element){ //sets stocks to the elements that are not in valid
+            return valid.indexOf(element) === -1;
+        });
+        console.log(name);
+        console.log(stocks);
+        console.log(stock);
+        console.log(valid);
+    });
+}
+
+
+//Posts information about the required stock
+router.post('/', function(req, res){
+	if(stock.filter(Boolean).indexOf(req.body.stock) === -1){ //if stock arr does not have the required stock
+		stock.filter(Boolean);
+		if(req.body.stock !== "undefined"){ //if stock is not equals to undefined
+			stock.push(req.body.stock);
+		}
+		stock = stock.filter(function(element){
+			return element !== req.body.name;
+		});
+		StockSymbolLookup.loadData()
+    	.then((data) => {
     	name = [];
         for(var i = 0; i < data.securities.length; i++){
         	for(var j = 0; j < stock.filter(Boolean).length; j++){
@@ -27,42 +67,16 @@ function loadData(){
                 }
             }
         });
-        stock = stock.filter(function(element){
+        stocks = stock.filter(function(element){
             return valid.indexOf(element) === -1;
         });
         console.log(name);
-        console.log(stock);
+        console.log(stocks);
         console.log(valid);
-    });
-}
-
-
-//Posts information about the required stock
-router.post('/', function(req, res){
-	if(stock.filter(Boolean).indexOf(req.body.stock) === -1){
-		stock.filter(Boolean);
-		if(req.body.stock !== "undefined"){
-			stock.push(req.body.stock);
-		}
-		stock = stock.filter(function(element){
-			return element !== req.body.name;
-		});
-		StockSymbolLookup.loadData()
-    .then((data) => {
-    	name = [];
-        for(var i = 0; i < data.securities.length; i++){
-        	for(var j = 0; j < stock.filter(Boolean).length; j++){
-        		stock = stock.filter(Boolean);
-            	if(stock[j] == data.securities[i].symbol){
-                	name.push([data.securities[i].symbol, data.securities[i].securityName]);
-            	}
-        	}
-		}
-        console.log(name);
-        console.log(stock);
         res.render('index',{
+        	invalid: stocks,
 			error: false,
-			data: stock.filter(Boolean),
+			data: stock,
 			names: name,
 			message: "The stock you are searching for already exists"
 		});
@@ -76,8 +90,9 @@ router.post('/', function(req, res){
 		});
 		loadData();
 		res.render('index',{
+			invalid: stocks,
 			error: true,
-			data: stock.filter(Boolean),
+			data: stock,
 			names: name,
 			message: "The stock you are searching for lalready exists"
 		});
@@ -95,6 +110,7 @@ router.get('/data/:stock', function(req, res){
 	}, function (err, quotes) {
 		if(err){
 			res.render('index',{
+				invalid: stocks,
 				error: true,
 				data: stock,
 				names: name,
@@ -114,6 +130,7 @@ router.get('/data/:stock', function(req, res){
 router.get("/", function(req, res){
 	loadData();
 	res.render("index", {
+		invalid: stocks,
 		error: false,
 		data: stock,
 		names: name,
